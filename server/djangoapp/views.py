86ec9d5369623de5a68e3f10fa42cba41ac13e9a
elmantib <elmantibrahim3@gmail.com>
2023-12-30 06:30:28 -0500

    firstcommit

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -15,11 +15,18 @@ logger = logging.getLogger(__name__)
 
 
 # Create your views here.
-
+def get_app(request):
+    context = {}
+    if request.method == "GET":
+        return render(request, 'djangoapp/app.html', context)
 
 # Create an `about` view to render a static about page
 # def about(request):
 # ...
+def about(request):
+    context = {}
+    if request.method == "GET":
+        return render(request, 'djangoapp/about.html', context)
 
 
 # Create a `contact` view to return a static contact page
